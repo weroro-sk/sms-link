ec5cc8739b523191c395d955bc3bf5913f99e6da
Dárius Bokor <darius.bokor@ringieraxelspringer.sk>
2017-10-26 01:47:22 +0200

    iter

--- app/test.js
+++ app/test.js
@@ -2,3 +2,13 @@
 // AdEsFRgthZ
 writeline(readline().match(/../g).map(i=>i.replace(/(.)(.)/,"$2$1")).join(''))
 
+// 8
+// OR
+// -8
+N=+readline()
+s=[]
+p=N<0?-1:1
+for(i=1;i<=p*N;i++)s[i-1]=i*p
+writeline(s.join())
+
+
